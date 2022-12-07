3b80e21391de90ff101a879a4609ee89c8c3cb83
Renard Bergson <renardrock@gmail.com>
2022-12-06 23:47:14 -0300

    feat: validando campos de inserção de dados

--- js/index.js
+++ js/index.js
@@ -1,32 +1,36 @@
 const $formulary = document.querySelector('#informations')
 const $inputs = document.querySelectorAll('#informations input')
-//const { name, monthlyPayment, fees, contributionTime } = $formulary
 
 $formulary.onsubmit = e => {
     e.preventDefault()
 
-    const $screenOne = document.querySelector('.screenOne')
-    const $screenTwo = document.querySelector('.screenTwo')
+    let isThereAnError = false
+    
+    $inputs.forEach(input => {
+        if (!input.value) {
+            isThereAnError = true
+            return input.classList.add('invalid')    
+        } 
+        input.classList.remove('invalid')
+    })
 
-    getInvisible($screenOne)
-    getVisible($screenTwo, 'flex')
-
-    const $returnBtn = document.querySelector('#returnBtn')
-    $returnBtn.onclick = () => {
-        getVisible($screenOne)
-        getInvisible($screenTwo)
-        resetFormulary()
+    if (!isThereAnError) {
+        const $screenOne = document.querySelector('.screenOne')
+        const $screenTwo = document.querySelector('.screenTwo')
+        
+        getInvisible($screenOne)
+        getVisible($screenTwo, 'flex')
+        
+        const $returnBtn = document.querySelector('#returnBtn')
+        
+        $returnBtn.onclick = () => {
+            getVisible($screenOne)
+            getInvisible($screenTwo)
+            resetFormulary()
+        }
     }
 }
 
 const getInvisible = item => item.style.display = 'none'
-
-function getVisible(item, display) {
-    if (display) {
-        item.style.display = display
-    } else {
-        item.style.display = 'block'
-    }
-}
-
+const getVisible = (item, display) => {display ? item.style.display = display : item.style.display = 'block'}
 const resetFormulary = () => $inputs.forEach(item => item.value = '')
