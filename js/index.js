2648d5c727263936dad989c130160713e4370485
Renard Bergson <renardrock@gmail.com>
2022-12-10 23:44:14 -0300

    feat: inserindo javascript
    inserindo requisição de API, funções para formatação dos dados e controle de visibilidadeÃ

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const $formulary = document.querySelector('#informations')
 const $inputs = document.querySelectorAll('#informations input')
 
+// ************************************************ FORMULARY CONTROLLER ************************************************
 $formulary.onsubmit = e => {
     e.preventDefault()
 
@@ -15,22 +16,114 @@ $formulary.onsubmit = e => {
     })
 
     if (!isThereAnError) {
-        const $screenOne = document.querySelector('.screenOne')
-        const $screenTwo = document.querySelector('.screenTwo')
-        
-        getInvisible($screenOne)
-        getVisible($screenTwo, 'flex')
-        
-        const $returnBtn = document.querySelector('#returnBtn')
-        
-        $returnBtn.onclick = () => {
-            getVisible($screenOne)
-            getInvisible($screenTwo)
-            resetFormulary()
-        }
+        request()
     }
 }
 
+// ******************************************** API REQUEST AND DATA CONSTRUCT ********************************************
+function request() {
+    const $payment = $formulary.monthlyPayment.value
+    const $fees = $formulary.fees.value
+    const $time = parseInt($formulary.contributionTime.value)
+
+    if (!$fees.includes(',') && !$fees.includes('.') && $fees.charAt(0) === '0') {
+        error()
+        return
+    }
+
+    const expression = { expr: `${paymentFormatter($payment)} * (((1 + ${feesFormatter($fees)}) ^ ${$time} - 1) / ${feesFormatter($fees)})` }
+
+    const configs = {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify( expression ) 
+    }
+
+    fetch('http://api.mathjs.org/v4', configs)
+        .then(getData)
+        .then(dataConstruct)
+        .catch(error)
+}
+
+function getData(data) {
+    if (data.status === 200) {
+        return data.json()
+    }
+}
+
+function dataConstruct(data) {    
+    const result = parseFloat(data.result)
+    const $resultMessage = document.querySelector('#resultMessage')
+
+    const { name, monthlyPayment, fees, contributionTime } = $formulary
+
+    $resultMessage.innerHTML = `Olá, ${name.value}! Investindo ${localeStrings(monthlyPayment.value)} todo mês, você acumulará ${localeStrings(result)} em ${contributionTime.value} meses, sob uma taxa de juros de ${fees.value.replace('.' , ',')} mensais.`
+
+    screenOperator()
+}
+
+function error() {
+    alert('Ops, ocorreu algum erro na requisição ou existem valores inseridos incorretamente!')
+}
+
+// ************************************************** SCREEN OPERATOR ***************************************************
+function screenOperator() {
+    const $screenOne = document.querySelector('.screenOne')
+    const $screenTwo = document.querySelector('.screenTwo')
+    
+    getInvisible($screenOne)
+    getVisible($screenTwo, 'flex')
+    
+    const $returnBtn = document.querySelector('#returnBtn')
+    
+    $returnBtn.onclick = () => {
+        getVisible($screenOne)
+        getInvisible($screenTwo)
+        //resetFormulary()
+    }
+}
+
+// ******************************************** DATA VERIFICATION FUNCTIONS ********************************************
+function paymentFormatter(item) {
+    let newPayment
+    
+    if (item.includes(',')) {
+        const replace = item.replace(',' , '.')
+        newPayment = parseFloat(replace)
+    } else {
+        newPayment = parseInt(item)
+    }
+
+    return newPayment
+}
+
+function feesFormatter(item) {
+    let newFees
+
+    if (item.includes('.') && !item.includes('%')) {
+        newFees = item + '%'
+    } else if (item.includes(',') && !item.includes('%')) {
+        console.log('tem virgula mas nao tem porcentagem')
+        newFees = item.replace(',' , '.') + '%'
+    } else if (item.includes(',') && item.includes('%')) {
+        newFees = item.replace(',' , '.')
+    } else {
+        newFees = item
+    }
+
+    return newFees
+}
+
+function localeStrings(num) {
+    if (typeof num === 'string') {
+        const number = parseFloat(num)
+        return number.toLocaleString("pt-BR", {style:"currency", currency:"BRL"})
+    } else {
+        return num.toLocaleString("pt-BR", {style:"currency", currency:"BRL"})
+    }
+}
+
+// ************************************ ITEM VISIBILITY AND VALUE CONTROL FUNCTIONS ************************************
 const getInvisible = item => item.style.display = 'none'
 const getVisible = (item, display) => {display ? item.style.display = display : item.style.display = 'block'}
 const resetFormulary = () => $inputs.forEach(item => item.value = '')
